c09b764e9a9eaa2388933a2a6c6e3dd164dfa297
Awedoba <severinasutani@gmail.com>
2022-12-08 14:12:40 -0800

    love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-
+/*print wihtout printf*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -c $CFILE 
+#include<stdio.h>
+/*print wiht printf*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+} 
