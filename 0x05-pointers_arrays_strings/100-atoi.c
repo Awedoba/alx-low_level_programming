354a55b09f3cdade66f2e69839d214dacbb5c0fc
Br0kinJester <50738523+Awedoba@users.noreply.github.com>
2022-12-19 23:24:23 +0000

    fvgbn

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,35 +7,36 @@
  *
  * Return: int
  */
+
 int _atoi(char *s)
 {
-int a = 0;
-unsigned int n = 0;
-int min = 1;
-int i = 0;
+    int c = 0;
+    unsigned int ni = 0;
+    int min = 1;
+    int isi = 0;
 
-while (s[a])
-{
-    if (s[a] == 45)
+    while (s[c])
     {
-        min *= -1;
+        if (s[c] == 45)
+        {
+            min *= -1;
+        }
+
+        while (s[c] >= 48 && s[c] <= 57)
+        {
+            isi = 1;
+            ni = (ni * 10) + (s[c] - '0');
+            c++;
+        }
+
+        if (isi == 1)
+        {
+            break;
+        }
+
+        c++;
     }
 
-    while (s[a] >= 48 && s[a] <= 57)
-    {
-        n = 1;
-        n = (n * 10) + (s[a] - '0');
-        a++;
-    }
-
-    if (n == 1)
-    {
-        break;
-    }
-
-    a++;
-}
-
-n *= min;
-return (n);
+    ni *= min;
+    return (ni);
 }
