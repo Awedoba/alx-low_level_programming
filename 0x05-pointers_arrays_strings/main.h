de53a313cb8ccb56e5139deea688a078644422da
Br0kinJester <50738523+Awedoba@users.noreply.github.com>
2022-12-19 22:25:19 +0000

    ad

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h
+ * Auth0r: Severin
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
