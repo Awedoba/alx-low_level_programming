e6c9898fb6b15a1a7263aa09c0dfd7e353aad363
Br0kinJester <50738523+Awedoba@users.noreply.github.com>
2023-01-03 23:27:59 +0000

    lkhj

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,17 +9,19 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int tl = 0;
+	int tr = 0;
+	int s = size * size;
 
-	unsigned int sum, sum1;
-
-	sum = 0;
-	sum1 = 0;
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < s; i += size + 1)
 	{
-		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
+		tl += a[i];
 	}
 
-	printf("%d, %d\n", sum, sum1);
+	for (i = size - 1; i < s - 1; i += size - 1)
+	{
+		tr += a[i];
+	}
+
+	printf("%d, %d\n", tl, tr);
 }
